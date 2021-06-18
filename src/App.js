e72bbc5eaa0232277273dd433b24fe1882b0e02f
Sea-Hummingbird <77937964+Sea-Hummingbird@users.noreply.github.com>
2021-06-18 16:12:17 -0500

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>🦉 Dictionary</h1>
+        <h1>Heliopedia</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Claudia Medel</footer>
     </div>
   );
 }

a50f261944c97361bb433a1a51bbd5beaf669018
Sea-Hummingbird <77937964+Sea-Hummingbird@users.noreply.github.com>
2021-06-21 17:38:59 -0500

    Added "Example" to the text

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,7 +9,7 @@ export default function Meaning(props){
                     <div key={index}>
                         <p>
                             {definition.definition} <br />
-                            <em>{definition.example}</em>
+                            <em>Example: {definition.example}</em>
                         </p>
                     </div>
                 );
